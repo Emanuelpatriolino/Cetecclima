81264562fdfd1d128a568604ed526124c5248ad9
Emanuelpatriolino <127460938+Emanuelpatriolino@users.noreply.github.com>
2024-02-12 17:47:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+var aaa = document.getElementById("form").addEventListener("submit",
+function (event){ 
+    event.preventDefault();
+
+var nome = document.getElementById("nome").value;
+var tel = document.getElementById("telefone").value;
+var rua = document.getElementById("rua").value;
+var bairro = document.getElementById("bairro").value;
+var procedimento = document.getElementById("procedimento").value;
+var endereço = rua +", "+ bairro;
+
+// alert("Olá " + nome + " suas informações foram coletadas, agora é só clicar e enviar.") 
+
+const mensagem = `Olá, gostaria de agendar uma visita para ${ procedimento}. Meu nome é ${ nome}, moro na ${ endereço}, meu whatsApp é ${ tel}, fico no aguardo! :) `;
+
+ const phonesalao = "+5585992209416";
+
+ const whatsappLink = `https://api.whatsapp.com/send?phone=${phonesalao}&text=${encodeURIComponent(mensagem)}`;
+
+ window.location.href = whatsappLink;
+
+var nome = document.getElementById("nome").value="";
+var tel = document.getElementById("telefone").value="";
+var rua = document.getElementById("rua").value="";
+var bairro = document.getElementById("bairro").value="";
+var procedimento = document.getElementById("procedimento").value="";
+
+}
+);
+
+
